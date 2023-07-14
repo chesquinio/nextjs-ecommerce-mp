e0a46926045b11f9a8e1a987beccc5f01d1936f4
francis <willenerfrancis0@gmail.com>
2023-07-14 15:39:34 -0300

    Ajusta para deploy

--- pages/_app.js
+++ pages/_app.js
@@ -13,9 +13,6 @@ const GlobalStyles = createGlobalStyle`
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
       <CartContextProvider>
         <GlobalStyles/>
         <Component {...pageProps} />
